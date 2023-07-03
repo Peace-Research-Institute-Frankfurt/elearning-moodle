7d59fb8a922ebc6c4ca9b6ae58047552a16136f0
Max Kohler <hello@maxkohler.com>
2023-07-03 09:41:40 +0200

    Fix matomo code

--- lib.php
+++ lib.php
@@ -20,7 +20,7 @@ function append_additionalhtmlhead()
   _paq.push([\'trackPageView\']);
   _paq.push([\'enableLinkTracking\']);
   (function() {
-    var u="//analytics.nonproliferation-elearning.eu";
+    var u="https://analytics.nonproliferation-elearning.eu/";
     _paq.push([\'setTrackerUrl\', u+\'piwik.php\']);
     _paq.push([\'setSiteId\', \'1\']);
     var d=document, g=d.createElement(\'script\'), s=d.getElementsByTagName(\'script\')[0];
