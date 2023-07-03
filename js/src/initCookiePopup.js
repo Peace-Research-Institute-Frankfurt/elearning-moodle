10ce71b8b746837acc432c75db6847bd0e4f8a50
Max Kohler <hello@maxkohler.com>
2023-07-03 13:27:40 +0200

    Fix analytics components

--- js/src/initCookiePopup.js
+++ js/src/initCookiePopup.js
@@ -1,28 +1,28 @@
-import Cookies from "js-cookie"
+import Cookies from "js-cookie";
 
 function setConsentCookie() {
-  Cookies.set("EU_COOKIE_LAW_CONSENT", "true", { expires: 30 })
+  Cookies.set("EU_COOKIE_LAW_CONSENT", "true", { expires: 30 });
 }
 
 export default function initCookiePopup() {
-  if (Cookies.get("EU_COOKIE_LAW_CONSENT") === "true") return
+  if (Cookies.get("EU_COOKIE_LAW_CONSENT") === "true") return;
 
-  const container = document.querySelector("#cookies-popup")
-  if (!container) return
+  const container = document.querySelector("#cookies-popup");
+  if (!container) return;
 
-  container.classList.add("active")
+  container.classList.add("active");
 
-  const acceptAllButton = container.querySelector("#js-allow-all")
-  const acceptNecessaryButton = container.querySelector("#js-allow-necessary")
+  const acceptAllButton = container.querySelector("#js-allow-all");
+  const acceptNecessaryButton = container.querySelector("#js-allow-necessary");
 
   acceptAllButton.addEventListener("click", (e) => {
-    setConsentCookie()
-    container.classList.remove("active")
-  })
+    setConsentCookie();
+    _paq.push(["rememberConsentGiven"]);
+    container.classList.remove("active");
+  });
 
   acceptNecessaryButton.addEventListener("click", (e) => {
-    _paq.push(["rememberConsentGiven"])
-    setConsentCookie()
-    container.classList.remove("active")
-  })
+    setConsentCookie();
+    container.classList.remove("active");
+  });
 }
