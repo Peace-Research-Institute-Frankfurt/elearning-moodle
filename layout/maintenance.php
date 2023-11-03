909acb2bd045529873af332ac3c592f22296cea4
Max Kohler <hello@maxkohler.com>
2023-11-03 20:20:47 +0000

    Don't import non-existent module

--- layout/maintenance.php
+++ layout/maintenance.php
@@ -6,30 +6,31 @@ echo $OUTPUT->doctype() ?>
 <html <?php echo $OUTPUT->htmlattributes(); ?>>
 
 <head>
-	<title><?php echo $OUTPUT->page_title(); ?></title>
-	<link rel="shortcut icon" href="<?php echo $OUTPUT->favicon(); ?>" />
-	<?php echo $OUTPUT->standard_head_html() ?>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo $OUTPUT->page_title(); ?></title>
+  <link rel="shortcut icon" href="<?php echo $OUTPUT->favicon(); ?>" />
+  <?php echo $OUTPUT->standard_head_html() ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body <?php echo $OUTPUT->body_attributes(); ?>>
 
-	<?php echo $OUTPUT->standard_top_of_body_html() ?>
+  <?php echo $OUTPUT->standard_top_of_body_html() ?>
 
-	<div id="page" class="container-fluid">
+  <div id="page" class="container-fluid">
 
-		<header id="page-header" class="clearfix">
-			<?php echo $html->heading; ?>
-		</header>
+    <header id="page-header" class="clearfix">
+      <?php echo $html->heading; ?>
+    </header>
 
-		<div id="page-content" class="row-fluid">
-			<section id="region-main" class="span12">
-				<?php echo $OUTPUT->main_content(); ?>
-			</section>
-		</div>
-	</div>
+    <div id="page-content" class="row-fluid">
+      <section id="region-main" class="span12">
+        <?php echo $OUTPUT->main_content(); ?>
+      </section>
+    </div>
+  </div>
 
-	<?php require_once(dirname(__FILE__) . '/includes/footer.php');  ?>
+  <?php // require_once(dirname(__FILE__) . '/includes/footer.php');
+  ?>
 
 </body>
 
